e0542139897d6761eb9647b8fb48b2be6724ee9f
kr1tik <kr1tik97chel@gmail.com>
2025-04-05 14:35:13 +0600

    Base ladder

--- scripts/nodes/player_input.gd
+++ scripts/nodes/player_input.gd
@@ -4,9 +4,11 @@ class_name PlayerInput
 
 
 var horizontal_input: float = 0.0
+var ladder_input: float = 0.0
 var is_jump_pressed: bool = false
 
 
 func _process(delta: float) -> void:
 	horizontal_input = Input.get_axis("move_left", "move_right")
 	is_jump_pressed = Input.is_action_just_pressed("jump")
+	ladder_input = Input.get_axis("ladder_up", "ladder_down")
--- scripts/nodes/player_movement.gd
+++ scripts/nodes/player_movement.gd
@@ -4,13 +4,21 @@ class_name PlayerMovement
 
 
 @export var speed: float = 0.0
+@export var ladder_speed: float = 0.0
 @export var jump_velocity: float = 0.0
 
 
-func apply_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
-	body.velocity.x = direction * speed
+func apply_horizontal_movement(body: CharacterBody2D, input: float) -> void:
+	body.velocity.x = input * speed
 
 
 func apply_jump_movement(body: CharacterBody2D) -> void:
 	if body.is_on_floor():
 		body.velocity.y -= abs(jump_velocity)
+		
+
+func apply_ladder_movement(body: CharacterBody2D, input: float) -> void:
+	if input != 0.0:
+		body.velocity.y = input * ladder_speed
+	else:
+		body.velocity.y = 0.0
