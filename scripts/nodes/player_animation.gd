31fe8d5eca5eae45d8fdb6d37eac4348c04e14dd
kr1tik <kr1tik97chel@gmail.com>
2025-04-07 04:22:51 +0600

    Player walk and idle animation

--- scripts/nodes/player_animation.gd
+++ scripts/nodes/player_animation.gd
@@ -1,3 +1,19 @@
 extends Node
 
 class_name PlayerAnimation
+
+
+@export var animated_sprite: AnimatedSprite2D
+
+
+func play_idle() -> void:
+	animated_sprite.play("idle")
+
+
+func play_walk(input) -> void:
+	if input < 0:
+		animated_sprite.flip_h = true
+	else:
+		animated_sprite.flip_h = false
+		
+	animated_sprite.play("walk")
