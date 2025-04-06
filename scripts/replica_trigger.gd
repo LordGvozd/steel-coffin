8730eff13972b7e057ca8bfabab87aa0a8423a99
kr1tik <kr1tik97chel@gmail.com>
2025-04-06 22:34:04 +0600

    Tilemap, new font

--- scripts/replica_trigger.gd
+++ scripts/replica_trigger.gd
@@ -8,7 +8,7 @@ var replica_system_node: ReplicaSystem
 
 
 func _ready() -> void:
-	replica_system_node = get_node("../Player/ReplicaSystem")
+	replica_system_node = get_node("../../Player/ReplicaSystem")
 
 
 func _process(delta: float) -> void:
@@ -25,12 +25,12 @@ func _on_area_entered(area: Area2D) -> void:
 		shader_material.shader = load("res://shaders/outline.gdshader")
 		shader_material.set_shader_parameter("width", 1)
 		shader_material.set_shader_parameter("outline_color", Color(255, 255, 255))
-		shader_material.set_shader_parameter("flickering_speed", 5)
+		shader_material.set_shader_parameter("flickering_speed", 6)
 		
-		self.material = shader_material
+		get_parent().material = shader_material
 	
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		is_player_in_range = false
-		self.material = null
+		get_parent().material = null
